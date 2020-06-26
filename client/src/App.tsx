85c318a4c743f939dda9ed78145af55c6f4ebccd
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2020-06-26 21:42:47 +0300

    Update parcel to use assets from blueprintjs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import {Navbar, Alignment, Icon} from '@blueprintjs/core';
+import './App.css';
+import '@blueprintjs/icons/resources/icons/icons-16.ttf';
+import '@blueprintjs/icons/resources/icons/icons-16.eot';
 
 const App: React.FC = () => (
-    <h2>Dapamaji.by</h2>
+    <div className="app">
+        <Navbar className="foo">
+            <Navbar.Group>
+                <Navbar.Heading>Dapamaji.by</Navbar.Heading>
+            </Navbar.Group>
+            <Navbar.Group align={Alignment.RIGHT}>
+                <Icon icon="user"/>
+            </Navbar.Group>
+        </Navbar>
+    </div>
 );
 
 export default App;
