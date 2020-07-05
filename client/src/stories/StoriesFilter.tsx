63b10079962a16174e9a7b819622da3804fdb41c
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2020-07-05 14:50:13 +0300

    Show filters on mobile layout

--- client/src/stories/StoriesFilter.tsx
+++ client/src/stories/StoriesFilter.tsx
@@ -30,7 +30,7 @@ const StoriesFilter: React.FC<{
       <InputGroup
         type="search"
         leftIcon={IconNames.SEARCH}
-        className="stories-filter__attribute"
+        className="stories-filter__attribute stories-filter__search"
         placeholder="Поиск"
         onChange={(evt: any) => {
           lastSearchVal = evt.target.value;
@@ -42,47 +42,49 @@ const StoriesFilter: React.FC<{
         }}
       ></InputGroup>
 
-      <CommonMultiselect
-        items={cities}
-        type={'multi'}
-        placeholder="Город"
-        className="stories-filter__attribute"
-        onChange={selected =>
-          set(
-            'Cities',
-            selected.map(item => item.value)
-          )
-        }
-      />
-
-      <CommonMultiselect
-        items={categories}
-        type={'multi'}
-        placeholder="Категория"
-        className="stories-filter__attribute"
-        onChange={selected =>
-          set(
-            'Categories',
-            selected.map(item => item.value)
-          )
-        }
-      />
-
-      <CommonMultiselect
-        items={sortOrders}
-        type="suggest"
-        placeholder="Сортировать"
-        className="stories-filter__attribute"
-        onChange={items => {
-          if (items.length > 0) {
-            onChange({
-              ...filters,
-              SortColumn: sortColumns[items[0].value],
-              SortDirection: sortDirections[items[0].value]
-            });
+      <div className="stories-filter__other-criteria">
+        <CommonMultiselect
+          items={cities}
+          type={'multi'}
+          placeholder="Город"
+          className="stories-filter__attribute"
+          onChange={selected =>
+            set(
+              'Cities',
+              selected.map(item => item.value)
+            )
           }
-        }}
-      />
+        />
+
+        <CommonMultiselect
+          items={categories}
+          type={'multi'}
+          placeholder="Категория"
+          className="stories-filter__attribute"
+          onChange={selected =>
+            set(
+              'Categories',
+              selected.map(item => item.value)
+            )
+          }
+        />
+
+        <CommonMultiselect
+          items={sortOrders}
+          type="suggest"
+          placeholder="Сортировать"
+          className="stories-filter__attribute"
+          onChange={items => {
+            if (items.length > 0) {
+              onChange({
+                ...filters,
+                SortColumn: sortColumns[items[0].value],
+                SortDirection: sortDirections[items[0].value]
+              });
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
